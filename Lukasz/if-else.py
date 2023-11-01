ffe49f0e7831e2fca35fd21a91a71e1ab891ad19
Lukasz <lukasz.marek.majerczyk@nav.no>
2023-11-01 08:38:12 +0100

    lister

---
+++ Lukasz/if-else.py
@@ -0,0 +1,7 @@
+age = int(input('Hvor gammel er du? '))
+if age < 18:
+    print('du er under 18 år gammel')
+if age > 18:
+    print('du er ')
+if age == 18:
+    print('du er 18 år gammel')
---
+++ Lukasz/lister.py
@@ -0,0 +1,3 @@
+numbers = [1,5, 102, 9, 42]
+for numbers in numbers:
+    print(numbers)
