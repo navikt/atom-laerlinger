4288138a952a6c673dbf676afdb5e714cfbd1412
Jonas Juvet <87018872+Jonas-Juvet@users.noreply.github.com>
2024-01-24 12:18:34 +0100

    Delete introduksjon/Herman directory

--- introduksjon/Herman/Hello.py
+++
@@ -1,3 +0,0 @@
-print('Hello World' + 'Navn')
-# Resultat:
-# Hello World Navn
--- introduksjon/Herman/land.py
+++
@@ -1,8 +0,0 @@
-
-
-countrys = ["Norway", "Sweden", "Denmark", "Finland", "Iceland", "United States", 
-            "Canada", "Mexico", "Brazil", "Argentina", "China", "Japan", "South Korea",
-          "India", "Australia", "New Zealand", "Russia", "Germany", "France", "United Kingdom"]
-
-for index, item in enumerate(countrys):
-    print(index, item)
